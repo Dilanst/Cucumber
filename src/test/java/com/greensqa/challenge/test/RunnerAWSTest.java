cc9d8d577d54dff1edad2976e9e2a0007336167a
Dilan Steven Mejia <mejiadilan@hotmail.com>
2020-02-14 14:11:29 -0500

    Update RunnerAWSTest.java

--- src/test/java/com/greensqa/challenge/test/RunnerAWSTest.java
+++ src/test/java/com/greensqa/challenge/test/RunnerAWSTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(	plugin = {"pretty","junit:report/report.xml"},
 					features = "src/test/resources",
-					glue =	"com.cafeto.challenge.po")
+					glue =	"com.greensqa.challenge.po")
 
 public class RunnerAWSTest {
 
